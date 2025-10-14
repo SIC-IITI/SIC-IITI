b41f4aaf24143406c74b26f759a0b0b4bb31536c
Yagami-light45 <harshithpasupuleti11@gmail.com>
2025-10-14 15:18:31 +0530

    "vite and tailwind setup"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -15,23 +15,21 @@ import Signup from './pages/Signup';
 
 export default function App() {
   return (
-    
-      <div className="min-h-screen flex flex-col bg-white">
-        <Navbar />
-        <main className="flex-1 max-w-7xl w-full mx-auto px-6 py-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/instruments" element={<Instruments />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/booking" element={<Booking />} />
-            <Route path="/usage-charges" element={<UsageCharges />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    
+    <div className="min-h-screen flex flex-col bg-white">
+      <Navbar />
+      <main className="flex-1 max-w-7xl w-full mx-auto px-6 py-12">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/instruments" element={<Instruments />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/usage-charges" element={<UsageCharges />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
