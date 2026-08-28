865b5b45785cbbb261d052742217c86d317cebb2
Anand Vivek <anand1234.vivek@gmail.com>
2026-08-28 13:17:32 +0530

    Create resolveImageUrl.js

---
+++ src/pages/Admin/components/resolveImageUrl.js
@@ -0,0 +1,8 @@
+import { API_BASE } from "../../lib/config";
+
+export function resolveImageUrl(path) {
+  if (!path) return "";
+  if (/^https?:\/\//i.test(path)) return path;
+  if (path.startsWith("/uploads/")) return `${API_BASE}${path}`;
+  return path;
+}
