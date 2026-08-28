bdfafd5916bb0d7c46d337c7093d2fa571fe2235
Anand Vivek <anand1234.vivek@gmail.com>
2026-08-28 13:43:17 +0530

    Update resolveImageUrl.js

--- src/pages/Admin/components/resolveImageUrl.js
+++ src/pages/Admin/components/resolveImageUrl.js
@@ -1,8 +1,7 @@
 import { API_BASE } from "../../../lib/config";
-
 export function resolveImageUrl(path) {
   if (!path) return "";
   if (/^https?:\/\//i.test(path)) return path;
   if (path.startsWith("/uploads/")) return `${API_BASE}${path}`;
-  return path;
+  return path.startsWith("/") ? path : `/${path}`;
 }
